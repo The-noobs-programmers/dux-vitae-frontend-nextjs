899bac894d1947abc9a190909d3666437066df8f
Kevin <kevin_mcp@outlook.com>
2022-11-19 20:12:05 -0300

    Refactor: Adicionando estilizacao e configuracoes globais

---
+++ src/styles/theme.ts
@@ -0,0 +1,66 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const activeLabelStyles = {
+  transform: "scale(0.85) translateY(-24px)",
+};
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "900": "#181b23",
+      "800": "#1f2029",
+      "700": "#353646",
+      "600": "#4b4d63",
+      "500": "#616480",
+      "400": "#797d9a",
+      "300": "#9699b0",
+      "200": "#b3b5c6",
+      "100": "#d1d2dc",
+      "50": "#eeeef2",
+    },
+  },
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.900",
+        color: "gray.50",
+      },
+    },
+  },
+  components: {
+    Form: {
+      variants: {
+        floating: {
+          container: {
+            _focusWithin: {
+              label: {
+                backgroundColor: "gray.800",
+                ...activeLabelStyles,
+              },
+            },
+            "input:not(:placeholder-shown) + label, .chakra-select__wrapper + label, textarea:not(:placeholder-shown) ~ label":
+              {
+                ...activeLabelStyles,
+              },
+            label: {
+              top: 0,
+              left: 0,
+              zIndex: 2,
+              position: "absolute",
+              backgroundColor: "gray.900",
+              pointerEvents: "none",
+              mx: 3,
+              px: 1,
+              my: 2,
+              transformOrigin: "left top",
+            },
+          },
+        },
+      },
+    },
+  },
+});
