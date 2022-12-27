3b05eec6bc31091b97727bff7c32fbb716c66b06
Kevin <kevin_mcp@outlook.com>
2022-12-27 15:21:42 -0300

    refactor: add new charts, with api data

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -7,100 +7,132 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 import { ApexOptions } from "apexcharts";
+import { GetServerSideProps } from "next";
 import dynamic from "next/dynamic";
+import nookies from "nookies";
+import { api } from "../../services/apiClient";
 
 const Chart = dynamic(() => import("react-apexcharts"), {
   ssr: false,
 });
 
-const options: ApexOptions = {
-  chart: {
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: false,
-    },
-    foreColor: theme.colors.gray[400],
-  },
-  grid: {
-    show: false,
-  },
-  tooltip: {
-    enabled: false,
-  },
-  xaxis: {
-    type: "datetime",
-    axisBorder: {
-      color: theme.colors.gray[600],
-    },
-    axisTicks: {
-      color: theme.colors.gray[600],
-    },
-    categories: [
-      "2021-03-18T00:00:00.000Z",
-      "2021-03-19T00:00:00.000Z",
-      "2021-03-20T00:00:00.000Z",
-      "2021-03-21T00:00:00.000Z",
-      "2021-03-22T00:00:00.000Z",
-      "2021-03-23T00:00:00.000Z",
-      "2021-03-24T00:00:00.000Z",
-      "2021-03-25T00:00:00.000Z",
-    ],
-  },
-  fill: {
-    opacity: 0.3,
-    type: "gradient",
-    gradient: {
-      shade: "dark",
-      opacityFrom: 0.7,
-      opacityTo: 0.3,
-    },
-  },
-};
-const series = [{ name: "series1", data: [41, 120, 10, 28, 61, 18, 100, 20] }];
+interface DashboardProps {
+  totalUsers: number[];
+  totalUserMonth: string[];
+  totalAppointments: number[];
+  totalAppointmentMonth: string[];
+  roles: string[];
+  totalUsersByRole: number[];
+}
 
-const barOptions: ApexOptions = {
-  chart: {
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: false,
-    },
-    foreColor: theme.colors.gray[400],
-  },
-  grid: {
-    show: false,
-  },
-  tooltip: {
-    enabled: false,
-  },
-  xaxis: {
-    type: "category",
-    axisTicks: {
-      color: theme.colors.gray[600],
-    },
-    categories: ["Enero", "Febrero", "Marzo", "Abril"],
-  },
-  fill: {
-    opacity: 0.3,
-    type: "gradient",
-    gradient: {
-      shade: "dark",
-      opacityFrom: 0.7,
-      opacityTo: 0.3,
-    },
-  },
-  colors: [theme.colors.pink[500]],
-};
-const barSeries = [{ name: "series2", data: [41, 120, 10, 28] }];
-
-export default function Dashboard() {
+export default function Dashboard({
+  totalUsers,
+  totalUserMonth,
+  totalAppointments,
+  totalAppointmentMonth,
+  roles,
+  totalUsersByRole,
+}: DashboardProps) {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  const userRegisterOptions: ApexOptions = {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+      foreColor: theme.colors.gray[400],
+    },
+    grid: {
+      show: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+    xaxis: {
+      type: "datetime",
+      axisBorder: {
+        color: theme.colors.gray[600],
+      },
+      axisTicks: {
+        color: theme.colors.gray[600],
+      },
+      categories: totalUserMonth,
+    },
+    fill: {
+      opacity: 0.3,
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        opacityFrom: 0.7,
+        opacityTo: 0.3,
+      },
+    },
+  };
+  const userRegisterSeries = [{ name: "series1", data: totalUsers }];
+
+  const appointmentOptions: ApexOptions = {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+      foreColor: theme.colors.gray[400],
+    },
+    grid: {
+      show: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+    xaxis: {
+      type: "category",
+      axisTicks: {
+        color: theme.colors.gray[600],
+      },
+      categories: totalAppointmentMonth,
+    },
+    fill: {
+      opacity: 0.3,
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        opacityFrom: 0.7,
+        opacityTo: 0.3,
+      },
+    },
+    colors: [theme.colors.pink[500]],
+  };
+  const appointmentSeries = [{ name: "series2", data: totalAppointments }];
+
+  const totalUserOption: ApexOptions = {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+      foreColor: theme.colors.gray[400],
+    },
+    labels: roles,
+    stroke: {
+      show: true,
+      curve: "smooth",
+      lineCap: "butt",
+      colors: undefined,
+      width: 2,
+      dashArray: 0,
+    },
+  };
+  const totalUserSeries = totalUsersByRole;
 
   return (
     <Flex flex="1" justify="center" overflowY="auto" mt="45px" mb="10px">
@@ -108,37 +140,100 @@ export default function Dashboard() {
         <SimpleGrid
           flex="1"
           gap="4"
-          minChildWidth="320px"
+          minChildWidth="420px"
           alignContent="flex-start"
         >
           <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
-              Inscritos da semana
+              Usuarios registrados
             </Text>
-            <Chart options={options} series={series} type="area" />
-          </Box>
-          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Inscritos da semana
-            </Text>
-            <Chart options={options} series={series} type="area" />
+            <Chart
+              options={userRegisterOptions}
+              series={userRegisterSeries}
+              type="area"
+            />
           </Box>
 
           <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
-              Taxa de abertura
+              Solicitudes por mes
             </Text>
-            <Chart options={options} series={series} type="area" />
+            <Chart
+              options={appointmentOptions}
+              series={appointmentSeries}
+              type="bar"
+            />
           </Box>
 
           <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
-              Taxa de abertura
+              Clientes x Nutricionistas
             </Text>
-            <Chart options={barOptions} series={barSeries} type="bar" />
+            <Chart
+              options={totalUserOption}
+              series={totalUserSeries}
+              type="pie"
+            />
           </Box>
         </SimpleGrid>
       </Flex>
     </Flex>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const { rut } = nookies.get(ctx);
+
+    //Getting total users by month and year
+    const users = await api.get("/api/dashboard/users/getByMonthAndYear");
+
+    let totalUsers = users.data.map((user: any) => user.totalUsers);
+
+    let totalUserMonth = users.data.map((user: any) => user.monthAndYear);
+
+    //Getting total appointments by month and year
+    const appointmentData = await api.get(
+      "/api/dashboard/appointment/getByMonthAndYear"
+    );
+
+    let totalAppointments = appointmentData.data.map(
+      (appointment: any) => appointment.totalAppointments
+    );
+
+    let totalAppointmentMonth = appointmentData.data.map(
+      (appointment: any) => appointment.monthAndYear
+    );
+
+    //Getting total client and nutritionist
+    const usersData = await api.get(
+      "/api/dashboard/users/getClientsVsNutritionist"
+    );
+
+    let roles = usersData.data.map((user: any) => user.role);
+
+    let totalUsersByRole = usersData.data.map((user: any) => user.total);
+
+    return {
+      props: {
+        totalUsers,
+        totalUserMonth,
+        totalAppointments,
+        totalAppointmentMonth,
+        roles,
+        totalUsersByRole,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        totalUsers: [],
+        totalUserMonth: [],
+        totalAppointments: [],
+        totalAppointmentMonth: [],
+        roles: [],
+        totalUsersByRole: [],
+      },
+    };
+  }
+};
